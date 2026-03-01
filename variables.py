33155c614527de94b8cc974201a2dc092a02cccd
vaishali-datascience <vaishalisangada08@gmail.com>
2026-03-01 23:44:34 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Basic variable examples
+
+name = "Vaishali"
+age = 20
+course = "Data Science"
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+
+# Taking user input
+user_name = input("Enter your name: ")
+print("Hello", user_name)
